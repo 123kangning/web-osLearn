b7d4fd1b26c2e6233d0a24a12b916e8888d30e4c
123kangning <3390142252@qq.com>
2023-03-04 16:59:44 +0800

    1

--- test.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "web-osLearn/models"
-
-func main() {
-	models.InitDB()
-	models.NewProjectDAO().AddProject(models.Project{Name: "Code", Author: "123kangning", Url: "https://github.com/123kangning/Code", Description: "练习代码"})
-}
